706f1b2b848c1a1b8a251f74f4a74520da307d31
OceanCT <yangzhichao_hit@163.com>
2022-08-16 18:50:33 +0800

    first commit

---
+++ monostack.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class monotype {
+public:
+    int con;
+    bool mono(monotype other) {
+        return other.con <= con ? true : false;
+    }
+};
+template <typename MONOTYPE>
+class monostack {
+private:
+    stack<MONOTYPE> sk;
+public:
+    void __preparePush(MONOTYPE k) {
+        while (!sk.empty() && !sk.top().mono(k))sk.pop();
+    }
+    void push(MONOTYPE k) {
+        __preparePush(k);
+        sk.push(k);
+    }
+    bool empty() { return sk.empty(); }
+    int size() { return sk.size(); }
+    void pop() { sk.pop(); }
+    MONOTYPE top() { return sk.top(); }
+};
+
+int main() {
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# C++ 个人板子
+
+st.cpp -- st表
+
+quickReadWrite.cpp -- 快读快写
+
+monostack.cpp -- 单调栈
